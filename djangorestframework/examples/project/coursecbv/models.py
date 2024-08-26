0151d46bc97e92b2f3a086228e65402f15b1db20
Virendra patel <patelvirendra62@gmail.com>
2024-08-26 15:43:44 +0530

    created model

--- djangorestframework/examples/project/coursecbv/models.py
+++ djangorestframework/examples/project/coursecbv/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Course(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, blank=False, null=False)
+    description = models.CharField(max_length=200)
+    thumbnail = models.ImageField(upload_to='thumbnails/')
+
+    def __str__(self):
+        return self.title
